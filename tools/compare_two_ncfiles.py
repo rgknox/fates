a984c4580145a95d3860863c98cde591fe557589
Ryan Knox <rgknox@lbl.gov>
2026-02-03 12:15:57 -0500

    Added formatting to the tool compare_two_ncfiles.py, so that it generates a patch file instead of just a diff

--- tools/compare_two_ncfiles.py
+++ tools/compare_two_ncfiles.py
@@ -2,6 +2,7 @@ import xarray as xr
 import json
 import numpy as np
 import argparse
+import datetime
 import sys
 import code  # For development: code.interact(local=dict(globals(), **locals()))
 import os
@@ -36,7 +37,7 @@ def get_structured_data(ds, var_name):
         
     return clean_nested_list(nested_list)
     
-def compare_netcdf_files(file1_path, file2_path, json_path):
+def compare_netcdf_files(file1_path, file2_path, diff_dic, json_path):
     
     # Load datasets
     ds1 = xr.open_dataset(file1_path)
@@ -59,22 +60,55 @@ def compare_netcdf_files(file1_path, file2_path, json_path):
     # 2. Variables in both that have different data values
     common_vars = vars2.intersection(vars1)
     different_data = [v for v in common_vars if not ds1[v].equals(ds2[v])]
-    
 
+    #code.interact(local=dict(globals(), **locals()))
+
+    numpft_ds1 = ds1.dims["fates_pft"]
+    numpft_ds2 = ds2.dims["fates_pft"]
+
+    if(numpft_ds1!=numpft_ds2):
+        print("The two parameter sets must have the same number of PFTS")
+        print("aborting")
+        exit(2)
+
+    if(numpft_ds1!=14):
+        print("WARNING: You are looking at diffs between two files")
+        print("The intention is to record these diffs, and then apply")
+        print("them to the default fates parameter file. This had 14")
+        print("PFTs last time we checked. So it is unclear how you")
+        print("would apply these diffs, since the PFT set is different")
+        print("If you are ok with this, override this exit")
+        exit(2)
+
+    diff_dic["pft_trim_list"] = list(range(1, numpft_ds1 + 1))
+    str_pft_list = ",".join(map(str, diff_dic["pft_trim_list"]))
+
+    diff_dic["parameters"] = {}
+    diff_dic["parameters"]["pft_parameters"] = {}
+    diff_dic["parameters"]["pft_parameters"][str_pft_list] = {}
+    diff_dic["parameters"]["non_pft_parameters"] = {}
+
+    print("\n\nThe following parameters were not in the base file")
+    print(" and will not be added to the patch file.")
+    print(" Feel free to manually add these to your patch if")
+    print(" you believe they are in the file you will apply")
+    print(" this patch to.")
     for var in unique_to_2:
-        result["new_parameters"][var] = get_structured_data(ds2, var)
+        print(f"New Parameter: {var}")
+        print(get_structured_data(ds2, var))
         
     for var in different_data:
-        result["changed_parameters"][var] = get_structured_data(ds2, var)
+        if("fates_pft" in ds2[var].dims):
+            diff_dic["parameters"]["pft_parameters"][str_pft_list][var] = get_structured_data(ds2, var)
+        else:
+            diff_dic["parameters"]["non_pft_parameters"][var] = get_structured_data(ds2,var)
 
 
-        
-    # Print as formatted JSON
-    #print(json.dumps(output_data, indent=4))
+    print("\nParsing differences complete, dictionary built, writing patch to file")
     with open(json_path, 'w') as outfile:
+        write_json.traverse_data(outfile,diff_dic)
 
-        write_json.traverse_data(outfile,result)
-        
+    print("\nComplete, exiting")
 
 def main():
     parser = argparse.ArgumentParser(description='Parse command line arguments to this script.')
@@ -85,8 +119,20 @@ def main():
     
     args = parser.parse_args()
 
+    time_str = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+    change_log = time_str+': modify_fates_paramfile_json.py'+' '.join(sys.argv[1:])
+
+    # This is the root of the new dictionary we are creating. This will be dumped into
+    # a json file.
+    diff_dic = {}
+    diff_dic["notes"] = "This patch file was created via:"+change_log
+
+    diff_dic["usage"] = f"This file should be passed as input to the batch patch python script in fates, from the parameter_files directory:, like so: (cd parameter_files;python tools/batch_patch_params.py --fin {args.jsonout})"
+
+    diff_dic["base_file"] = "UNSPECIFIED_BASEFILE.json"
+    diff_dic["new_file"]  = "UNSPECIFIED_NEWFILE.json"
     
-    compare_netcdf_files(args.basefile, args.newfile, args.jsonout)
+    compare_netcdf_files(args.basefile, args.newfile, diff_dic, args.jsonout)
     
 
 if __name__ == "__main__":
